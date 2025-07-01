0a25b1d81877f12f20a972424319119492f8f1ef
Mahdy Hasan <mahdyhasan3010@gmail.com>
2025-07-01 23:57:34 +0600

    mjhdcvs

---
+++ src/components/Work.jsx
@@ -0,0 +1,15 @@
+import Container from "./Container"
+import Heading from "./Heading"
+
+const Work = () => {
+  return (
+<>
+<div className="py-[100px] bg-amber-300">
+    <Container>
+        <Heading text={"Our Services"} className={"text-[40px] font-bold font-inter text-menuC text-center"} as="h3"/>
+    </Container>
+</div>
+</>  )
+}
+
+export default Work
