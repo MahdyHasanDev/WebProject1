5ad1f9c96df6748a1d5d0e450141af7a8fdba058
Mahdy Hasan <mahdyhasan3010@gmail.com>
2025-07-01 16:20:09 +0600

    dfkljh

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 
 import './App.css'
 import Banner from './components/Banner'
+
 import Header from './components/layout/Header'
+import Heading from './components/layout/Heading'
 
 function App() {
 
   return (
-   <>
+    <>
+   
    <Header/>
    <Banner/>
+  <Heading text={"Hewllow"}/>
+   
    </>
   )
 }
